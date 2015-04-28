d39c0eccb459ef8f2383262acd2fddc06e31859b
Jon Chesterfield <jonathanchesterfield@gmail.com>
2015-04-28 23:07:47 +0100

    Added test/test_fixture/check macros to LegacyTest.hpp

--- LegacyTest.hpp
+++ LegacyTest.hpp
@@ -1,10 +1,21 @@
-#ifndef INCLUDED_LEGACY_TEST_H
-#define INCLUDED_LEGACY_TEST_H
+#ifndef INCLUDED_LEGACY_TEST_HPP
+#define INCLUDED_LEGACY_TEST_HPP
 
-#include "catch.h"
+#include "catch.hpp"
 
+#ifdef TEST
+#undef TEST
+#endif
+#define TEST(X) TEST_CASE(#X)
 
+#ifdef TEST_FIXTURE
+#undef TEST_FIXTURE
+#endif
+#define TEST_FIXTURE(class,X) TEST_CASE_METHOD(class,#X)
 
+#ifdef CHECK_EQUAL
+#undef CHECK_EQUAL
+#endif
+#define CHECK_EQUAL(LHS,RHS) CHECK(LHS == RHS)
 
-
-#endif // INCLUDED_LEGACY_TEST_H
+#endif // INCLUDED_LEGACY_TEST_HPP
