8fa3872b8ae1954120e0221e952ba1603450bd05
Jon Chesterfield <jonathanchesterfield@gmail.com>
2015-04-28 23:21:17 +0100

    Added suite, check_throw and a little documentation

--- LegacyTest.hpp
+++ LegacyTest.hpp
@@ -3,19 +3,37 @@
 
 #include "catch.hpp"
 
+
+// Convert a valid_c89_identifier used as a test name into a string literal
+// for TEST() and TEST_FIXTURE() macros.
+
 #ifdef TEST
 #undef TEST
 #endif
-#define TEST(X) TEST_CASE(#X)
+#define TEST(Name) TEST_CASE(#Name)
 
 #ifdef TEST_FIXTURE
 #undef TEST_FIXTURE
 #endif
-#define TEST_FIXTURE(class,X) TEST_CASE_METHOD(class,#X)
+#define TEST_FIXTURE(class,Name) TEST_CASE_METHOD(class,#Name)
+
+// A suite can be approximated by a namespace
+
+#ifdef SUITE
+#undef SUITE
+#endif
+#define SUITE(Name) namespace Suite##Name
+
+// Wrap CHECK macros
 
 #ifdef CHECK_EQUAL
 #undef CHECK_EQUAL
 #endif
 #define CHECK_EQUAL(LHS,RHS) CHECK(LHS == RHS)
+
+#ifdef CHECK_THROW
+#undef CHECK_THROW
+#endif
+#define CHECK_THROW(Expr, Exception) CHECK_THROWS_AS(Expr, Exception)
 
 #endif // INCLUDED_LEGACY_TEST_HPP
